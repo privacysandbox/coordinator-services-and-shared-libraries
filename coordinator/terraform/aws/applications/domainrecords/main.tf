cff14856b4fe3948d89c89bdd9494d9414c49750
Privacy Sandbox Team <noreply@google.com>
2023-08-28 09:20:36 -0400

    Release version v1.2.0
    GitOrigin-RevId: 56e1d9d129c706fe651fdb3cc7db55164831e915

---
+++ coordinator/terraform/aws/applications/domainrecords/main.tf
@@ -0,0 +1,31 @@
+# Adds necessary records to route53 for domain before creating environments
+# Only needs to be used once
+
+terraform {
+  required_version = "~> 1.2.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name
+}
+
+# Create CAA record for domain. Required for DNS validation
+resource "aws_route53_record" "caa_record" {
+  zone_id = aws_route53_zone.hosted_zone.zone_id
+  name    = var.domain_name
+  type    = "CAA"
+  records = [
+    "0 issue \"amazon.com\""
+  ]
+  ttl = 60
+}
